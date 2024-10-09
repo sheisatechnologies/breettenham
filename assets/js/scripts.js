090d3aab9ca1c49b2d087e73910d6c73cfa6184d
sheisa technologies <120563057+sheisatechnologies@users.noreply.github.com>
2024-10-09 12:09:58 +0530

    ok

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,84 +1,67 @@
-gsap.from(".red-box-home1", {
-    x: -800,        // Start from 800px to the right
-    opacity: 0,    // Start with zero opacity
-    duration: 3,   // Duration of 3 seconds
-  });
-
-  gsap.from(".red-box-home1 img", {
-    x: -300,        // Start from 800px to the right
-    opacity: 0,    // Start with zero opacity
-    duration: 3,   // Duration of 3 seconds
-    delay:1
-  });
-
-
-  gsap.from(".yellow-box-home2", {
-    x: -800,        // Start from 800px to the right
-    opacity: 0,    // Start with zero opacity
-    duration: 3,   // Duration of 3 seconds
-  });
-
-  gsap.from(".yellow-box-home2 img", {
-    x: -300,        // Start from 800px to the right
-    opacity: 0,    // Start with zero opacity
-    duration: 3,   // Duration of 3 seconds
-    delay:1
-  });
-
-
-  gsap.from(".blue-box-buil1", {
-    x: 800,        // Start from 800px to the right
-    opacity: 0,    // Start with zero opacity
-    duration: 3,   // Duration of 3 seconds
-  });
-
-  gsap.from(".blue-box-buil1 img", {
-    x: 800,        // Start from 800px to the right
-    opacity: 0,    // Start with zero opacity
-    duration: 3,   // Duration of 3 seconds
-    delay:1
-  });
-
-
-  gsap.from(".orange-box-buil3", {
-    x: 800,        // Start from 800px to the right
-    opacity: 0,    // Start with zero opacity
-    duration: 3,   // Duration of 3 seconds
-  });
-
-  gsap.from(".orange-box-buil3 img", {
-    x: 800,        // Start from 800px to the right
-    opacity: 0,    // Start with zero opacity
-    duration: 3,   // Duration of 3 seconds
-    delay:1
-  });
-
-
-  
 
 
 
-  
-  
-  // gsap.to(".h-slide1-img", {
-  //   x: -300,        // Start from 800px to the right
-  //   opacity: 0,    // Start with zero opacity
-  //   duration: 3,   // Duration of 3 seconds
-  //   delay:1
-  // });
+ // Home page slider 1 //
+
+$(document).ready(function(){
+    $('.slider-1').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 5000,
+        cssEase:"cubic-bezier(0.2, 0.4, 0.4, 0.9)",
+        swipeToSlide: true,
+        accessibility: true
+    });
+});
+ // Home page slider 1 //
 
 
-  // gsap.to(".slide-con1", {
-  //   x: -800,        // Start from 800px to the right
-  //   opacity: 0,    // Start with zero opacity
-  //   duration: 3,   // Duration of 3 seconds
-  // });
 
-  // const tl = gsap.timeline({ defaults: { duration: 1 } });
+ // This code use for hide and show the top header //
 
-  // // Define animations
-  // tl.from(".slide-con1", { x: -800, opacity: 0, duration: 2,  }) // Animate the box
-  //   .from(".h-slide1-img",  { x: -300, opacity: 0, duration: 2 }) // Animate the circle at the same time as the box
-  //   .to(".slide-con1", { x: -800, opacity: 0, duration: 2}) // Fade out the box
-  //   .to(".h-slide1-img", { x: -300, opacity: 0, duration: 2}); // Fade out the circle
+$(window).on('scroll', function() {
+    if ($(this).scrollTop() > 50) {
+        $('#normalLogo').hide();
+        $('#fixedLogo').show();
+    } else {
+        $('#normalLogo').show();
+        $('#fixedLogo').hide();
+    }
+});
 
+ // This code use for hide and show the top header //
+
+
+
+
+
+
+
+ // Floor plan Custom Js for calculation //
+   
+ 
+ let totalSum = 0;
+
+    // Function to toggle the value of the clicked cell between adding and subtracting
+    function toggleValue(cell) {
+        let cellValue = parseFloat(cell.textContent.replace(/,/g, '')); // Remove commas and convert to number
+
+        // Check if the cell has been clicked previously (toggle on/off)
+        if (cell.classList.contains('clicked')) {
+            // If cell is already clicked, subtract its value from the total
+            totalSum -= cellValue;
+            cell.classList.remove('clicked'); // Remove the clicked class to indicate deselection
+        } else {
+            // If cell is not clicked, add its value to the total
+            totalSum += cellValue;
+            cell.classList.add('clicked'); // Add the clicked class to indicate selection
+        }
+
+        // Update the sum display
+        document.getElementById('sumCell').textContent = totalSum.toLocaleString();
+    }
+
+ // Floor plan Custom Js for calculation //
